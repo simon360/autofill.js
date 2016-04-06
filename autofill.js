1326a7e8621479d498cb7ac29a80d2e1d8aca565
Simon Andrews <me@simonster.net>
2016-04-07 00:13:18 +0100

    fix: Wrap everything into a function with 'use strict'.
    Closes AustinPaquette/autofill.js#2

--- autofill.js
+++ autofill.js
@@ -1,55 +1,59 @@
-var params = [];
-var autofill = false;
+(function($) {
+    'use strict';
+    
+    var params = [];
+    var autofill = false;
 
-if (location.search) {
-    var parts = location.search.substring(1).split('&');
+    if (location.search) {
+        var parts = location.search.substring(1).split('&');
 
-    for (var i = 0; i < parts.length; i++) {
-        var nv = parts[i].split('=');
-        if (!nv[0]) continue;
+        for (var i = 0; i < parts.length; i++) {
+            var nv = parts[i].split('=');
+            if (!nv[0]) continue;
 
-        if (nv[0] === 'autofill' && nv[1] == 'true') {
-            autofill = true;
-        }
+            if (nv[0] === 'autofill' && nv[1] == 'true') {
+                autofill = true;
+            }
 
-        params.push({key: nv[0], value: (nv[1] || true)});
-    }
-}
-
-if (autofill) {
-    for (var i = 0; i < params.length; i++) {
-        var param = params[i];
-        formAutoPopulate(param.key, param.value);
-    }
-
-    history.pushState('data', '', window.location.pathname);
-}
-
-function formAutoPopulate(name, value) {
-    if (name != 'autofill') {
-        var populated = false;
-        var field = $('[name="'+ name +'"]');
-        var type = $('[name="'+ name +'"]').prop("tagName").toLowerCase();
-
-        name = decodeURIComponent(name);
-        value = decodeURIComponent(value);
-
-        if (type == 'input') {
-            field.val(value);
-            populated = true;
-        }
-
-        if (type == 'textarea') {
-            field.html(value);
-            populated = true;
-        }
-
-        if (type == 'select') {
-            //
-        }
-
-        if (populated) {
-            field.attr('autopopulated', 'true');
+            params.push({key: nv[0], value: (nv[1] || true)});
         }
     }
-}
+
+    if (autofill) {
+        for (var i = 0; i < params.length; i++) {
+            var param = params[i];
+            formAutoPopulate(param.key, param.value);
+        }
+
+        history.pushState('data', '', window.location.pathname);
+    }
+
+    function formAutoPopulate(name, value) {
+        if (name != 'autofill') {
+            var populated = false;
+            var field = $('[name="'+ name +'"]');
+            var type = $('[name="'+ name +'"]').prop("tagName").toLowerCase();
+
+            name = decodeURIComponent(name);
+            value = decodeURIComponent(value);
+
+            if (type == 'input') {
+                field.val(value);
+                populated = true;
+            }
+
+            if (type == 'textarea') {
+                field.html(value);
+                populated = true;
+            }
+
+            if (type == 'select') {
+                //
+            }
+
+            if (populated) {
+                field.attr('autopopulated', 'true');
+            }
+        }
+    }
+})(jQuery);
